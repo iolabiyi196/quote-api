f96fc7161d89506278fc2cad2d77ba21542577b3
iolabiyi <79932461+iolabiyi@users.noreply.github.com>
2023-01-18 21:23:46 -0500

    added get requests to the server.js file

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require('./data')
+const { getRandomElement } = require('./utils')
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.get()
+app.get('/api/quotes/random', (req, res) => {
+  res.send({ quote: getRandomElement(quotes) })
+})
+
+app.get('/api/quotes', (req, res) => {
+  const query = req.query.person
+  if (query !== undefined) {
+    const quotesByPerson = quotes.filter((quote) => quote.person === query)
+    res.send({ quote: quotesByPerson })
+  } else {
+    res.send({ quotes: quotes })
+  }
+})
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-});
+  console.log(`listening on port ${PORT}`)
+})
